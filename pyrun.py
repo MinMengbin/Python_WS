6a02c5aeda2d098258d257e668a44587471058ed
Mike Min <prlatlab@gmail.com>
2018-10-18 13:32:54 +1030

    uncomment import math and mathplotlib

--- pyrun.py
+++ pyrun.py
@@ -1,10 +1,12 @@
-# import math
-# import matplotlib.pyplot as plt
-# import matplotlib as mpl
-# import numpy as np
+# this file is used to test all python related coding
 
-# x = np.linspace(0,20,100)
-# plt.plot(x, np.sin(x))
-# plt.show()
+import math
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0,20,100)
+plt.plot(x, np.sin(x))
+plt.show()
 
 print(14//3)
